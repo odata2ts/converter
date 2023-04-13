bd54eeb9872b7a80efba7744922a8303a1fd2a30
texttechne <gedankengut@gmx.de>
2023-04-13 11:40:02 +0200

    refactor: remove IdentityConverter (#2)

--- packages/converter-runtime/test/ChainedCoverter.test.ts
+++ packages/converter-runtime/test/ChainedCoverter.test.ts
@@ -1,6 +1,6 @@
 import { numberToStringConverter, stringToPrefixModelConverter } from "@odata2ts/test-converters";
 
-import { createChain, getIdentityConverter } from "../src";
+import { createChain } from "../src";
 
 describe("ChainedConverter Test", () => {
   const toTest = createChain(numberToStringConverter, stringToPrefixModelConverter);
@@ -10,12 +10,4 @@ describe("ChainedConverter Test", () => {
     expect(toTest.convertTo({ prefix: "PREFIX", value: "22" })).toBe(22);
   });
 
-  test("Chained Converter: one more chaining", () => {
-    const chained = createChain(numberToStringConverter, getIdentityConverter())
-      .chain(getIdentityConverter())
-      .chain(stringToPrefixModelConverter);
-
-    expect(toTest.convertFrom(3)).toStrictEqual({ prefix: "PREFIX_", value: "3" });
-    expect(toTest.convertTo({ prefix: "PREFIX", value: "22" })).toBe(22);
-  });
 });
