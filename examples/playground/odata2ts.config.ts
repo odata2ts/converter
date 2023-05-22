beeeccd0bdeafb89064e93b978a85fd066e8560b
texttechne <gedankengut@gmx.de>
2023-05-22 16:56:29 +0200

    feat(example): example & int-test for v2Tov4 and common converter

---
+++ examples/playground/odata2ts.config.ts
@@ -0,0 +1,20 @@
+import { ConfigFileOptions, EmitModes, Modes } from "@odata2ts/odata2ts";
+
+const config: ConfigFileOptions = {
+  mode: Modes.qobjects,
+  emitMode: EmitModes.ts,
+  prettier: true,
+  converters: [
+    "@odata2ts/converter-v2-to-v4",
+    { use: ["timeToDurationConverter"], module: "@odata2ts/converter-v2-to-v4" },
+    "@odata2ts/converter-common",
+  ],
+  services: {
+    test: {
+      source: "src/test.xml",
+      output: "build",
+    },
+  },
+};
+
+export default config;
