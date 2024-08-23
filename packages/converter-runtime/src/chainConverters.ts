be6d5f2a9aaac3179f6a118b72a9bc3172eb448d
Hubert Drecker <gedankengut@gmx.de>
2024-08-23 18:18:49 +0200

    refactor: revert to commonjs (#22)
    * Revert "fix: add ".js" to all relative imports (#21)"
    
    This reverts commit 808d0217edf9b8b90062e412ddc8e956c865c01b.
    
    * refactor: all libs should export commonjs for max compat

--- packages/converter-runtime/src/chainConverters.ts
+++ packages/converter-runtime/src/chainConverters.ts
@@ -1,5 +1,5 @@
 import { ChainableValueConverter, ValueConverter } from "@odata2ts/converter-api";
-import { ChainedConverter } from "./ChainedConverter.js";
+import { ChainedConverter } from "./ChainedConverter";
 
 export function createChain<Source, Temp, Target>(
   converterA: ValueConverter<Source, Temp>,
--- packages/converter-ui5-v2/src/index.ts
+++ packages/converter-ui5-v2/src/index.ts
@@ -5,7 +5,7 @@ import {
   dateTimeToDateTimeOffsetConverter,
   stringToNumberConverter,
 } from "@odata2ts/converter-v2-to-v4";
-import { timeToMsDurationConverter } from "./TimeToMsDurationConverter.js";
+import { timeToMsDurationConverter } from "./TimeToMsDurationConverter";
 
 export { MsDuration } from "./TimeToMsDurationConverter";
 
