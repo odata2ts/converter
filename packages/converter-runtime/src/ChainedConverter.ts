4e69fe5b82a0fbe0ec383b812722764d8dca21a4
texttechne <gedankengut@gmx.de>
2025-02-08 23:58:38 +0100

    fix: chained converters must respect & pass along options

--- packages/converter-runtime/src/ChainedConverter.ts
+++ packages/converter-runtime/src/ChainedConverter.ts
@@ -1,4 +1,4 @@
-import { ChainableValueConverter, ParamValueModel, ValueConverter } from "@odata2ts/converter-api";
+import {ChainableValueConverter, ConverterOptions, ParamValueModel, ValueConverter} from "@odata2ts/converter-api";
 
 export class ChainedConverter<FromType, IntermediateType, ToType> implements ChainableValueConverter<FromType, ToType> {
   public readonly id = "ChainedConverter";
@@ -13,12 +13,12 @@ export class ChainedConverter<FromType, IntermediateType, ToType> implements Cha
     this.to = converter2.to;
   }
 
-  public convertFrom(value: ParamValueModel<FromType>): ParamValueModel<ToType> {
-    return this.converter2.convertFrom(this.converter.convertFrom(value));
+  public convertFrom(value: ParamValueModel<FromType>, options?: ConverterOptions): ParamValueModel<ToType> {
+    return this.converter2.convertFrom(this.converter.convertFrom(value, options), options);
   }
 
-  public convertTo(value: ParamValueModel<ToType>): ParamValueModel<FromType> {
-    return this.converter.convertTo(this.converter2.convertTo(value));
+  public convertTo(value: ParamValueModel<ToType>, options?: ConverterOptions): ParamValueModel<FromType> {
+    return this.converter.convertTo(this.converter2.convertTo(value, options), options);
   }
 
   public chain<T>(converterToChain: ValueConverter<ToType, T>): ChainableValueConverter<FromType, T> {
